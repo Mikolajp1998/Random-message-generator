b6d47a5353aaf8d9ce84bb4128357a67c8803db3
Mikolaj <mikolajpisak@Gmail.com>
2024-02-15 02:28:45 +0000

    no changes

---
+++ Random-message-generator-main/README.md
@@ -0,0 +1,2 @@
+# Random-message-generator
+##this mmessage generator will generate random messages each time the code is run.
---
+++ Random-message-generator-main/message-generator.js
@@ -0,0 +1,6 @@
+const message1 =['hello','Good day', 'Hey','Greetings','What a lovely day today!'];
+const message2 =['What will the random message be today?','lets see what message we get today!',"let's brigthen up our day with a new message"];
+const message3 =['Today will be a good day','You will acheive your goals today','today may be tough, but you will get through it!'];
+const message4 =['hope this helped to cheer you up!', 'You will find the motivation needed to get through today!','There\'s always tomorrow!'];
+
+let random_message = [];
