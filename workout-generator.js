4d265711bac042e801c5a78f434a792fdbdf1576
Mikolaj <mikolajpisak@Gmail.com>
2024-02-17 18:37:57 +0000

    Workout Generator Changes

--- workout-generator.js
+++ workout-generator.js
@@ -1,41 +1,56 @@
 const workout = {
     exercises: ['Bench Press', 'Deadlift', 'Squat', 'Dumbell Press','Leg Press','Tricep Pull-Down','Bicep Curl','Curl Press','Inclince Dumbell Press'],
-    quantity_reps: [2,4,8,10,15,20],
-    quantity_set: [1,2,3,4,5],
+    quantity_reps: [10,15,20],
+    quantity_set: [3,4,5],
 }
 function randomGenerator(num){
-    return Math.floor(Math.random() * num)
+    return Math.floor(Math.random() * num + 1)
 };
 
-const workoutGenerated = [];
 let workoutAmount = Math.ceil(Math.random() * workout.exercises.length)
-
-for (let i = 1; i < workoutAmount; i++) {
-    let exercise = workout.exercises[randomGenerator(workout.exercises.length)];
-    if (exercise !== workoutGenerated[i]) {
-      workoutGenerated.push(`Your workout number ${i} today will be: ${exercise}`);
-    }
+const workout2 = [];
+for (let i = 0; i < workoutAmount; i++){
+  let exercise = workout.exercises[randomGenerator(workout.exercises.length)];
+  workout2.push(exercise);
   
-    for (let x in workout) {
-      let options = randomGenerator(workout[x].length);
-      switch (x) {
-        case 'exercises':
-          break;
-        case 'quantity_reps':
-          workoutGenerated.push(`You will do ${workout[x][options]} reps`);
-          break;
-        case 'quantity_set':
-          workoutGenerated.push(`And You will do ${workout[x][options]} sets`);
-          break;
-        default:
-          workoutGenerated.push('There is not enough info.');
-      }
-    }
+}
+const reps = [];
+const sets = [];
+for(let x in workout){
+  let options = randomGenerator(workout[x].length);
+  switch (x){
+    case 'quantity_reps':
+      reps.push(workout[x][options]);
+      break;
+    case 'quantity_set':
+      sets.push(workout[x][options]);
+      break;
   }
+}
+////function to remove duplicate arrays from the randomly generated list of exercises 
+function removeDuplicates(arr){
+  let unique = [];
+  arr.forEach(element => {
+      if (!unique.includes(element)) {
+          unique.push(element);
+      }
+  });
+  return unique;
+}
 
+
+let newWorkout = removeDuplicates(workout2);
+
+const completeWorkout = [];
+function addText(arr){
+ 
+  for(let i = 1; i < arr.length; i++){
+    completeWorkout.push(`Your workout number ${i} is: ${arr[i]}. You will do ${randomGenerator(sets)} sets of ${randomGenerator(reps)} reps.` );
+  }
+}
+addText(newWorkout);
 function format (work){
-    const formatted = workoutGenerated.join('\n')
-    console.log(formatted)
+    const formatted = completeWorkout.join('\n')
+    console.log(completeWorkout)
 };
-
-format(workoutGenerated);
+format(completeWorkout);
